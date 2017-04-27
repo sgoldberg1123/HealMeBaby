0ad5fef05b70cf70dbeb9fba8a434e535802a89f
schmidtp070 <schmidtp0740@gmail.com>
2017-04-27 08:37:01 -0500

    deleted RestService folder

--- RestService/config/dbConn.js
+++
@@ -1,5 +0,0 @@
-//creating user data database connection
-var mysql = require('mysql');
-var config = require('./config');
-var dbConn = mysql.createConnection(config.dbConnection);
-module.exports = dbConn;
